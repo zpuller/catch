a64545e3fe7156b424b1e5d629434910951bfc08
Zach Puller <zach.puller@gmail.com>
2022-05-06 23:04:34 -0700

    init frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
