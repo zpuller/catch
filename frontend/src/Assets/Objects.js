cd277e31821c3fa912f5d2d225a8413a79b3cfdd
Zach Puller <zach.puller@gmail.com>
2022-05-07 23:28:33 -0700

    add a room

--- frontend/src/Assets/Objects.js
+++ frontend/src/Assets/Objects.js
@@ -1,11 +1,20 @@
 import * as THREE from 'three'
 
+
+
 const init = (scene) => {
+    {
+        const geometry = new THREE.BoxGeometry(32, 16, 32)
+        const material = new THREE.MeshPhysicalMaterial({ color: '#eb9048', side: THREE.BackSide })
+        const mesh = new THREE.Mesh(geometry, material)
+        mesh.position.fromArray([0, 8, 0])
+        scene.add(mesh)
+    }
+
     const geometry = new THREE.SphereGeometry(0.2, 16, 16)
     const material = new THREE.MeshPhysicalMaterial({ color: '#04f679' })
     const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.y = 1.6
-    mesh.position.z = -1
+    mesh.position.fromArray([0, 1.6, -1])
     scene.add(mesh)
 
     return mesh
