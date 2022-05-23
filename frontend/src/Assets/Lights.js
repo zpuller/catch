e76c8cb68ff41a0892a288a19a4840083f7e83da
Zach Puller <zach.puller@gmail.com>
2022-05-22 23:26:44 -0700

    messing with lights

--- frontend/src/Assets/Lights.js
+++ frontend/src/Assets/Lights.js
@@ -6,8 +6,13 @@ export default class Lights {
         group.add(new THREE.HemisphereLight(0x606060, 0x404040));
 
         const light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(1, 1, 1).normalize();
-        group.add(light);
+        light.position.set(-2, 2, -1)
+
+        const plight = new THREE.PointLight(0xffffff, 1, 4, .5)
+        plight.position.set(2, 3, -1)
+        group.add(plight)
+
+        group.add(light)
 
         return group
     }
