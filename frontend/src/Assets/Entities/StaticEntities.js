119a7d87457184228c63c0a15805886124734997
Zach Puller <zach.puller@gmail.com>
2022-05-27 09:14:20 -0700

    cleaning

--- frontend/src/Assets/Entities/StaticEntities.js
+++ frontend/src/Assets/Entities/StaticEntities.js
@@ -1,75 +1,37 @@
 import * as CANNON from 'cannon-es'
 
+const createBody = (conf) => {
+    const { s, p, q } = conf
+    return new CANNON.Body({
+        type: CANNON.Body.STATIC,
+        shape: new CANNON.Box(new CANNON.Vec3(s.w, s.h, s.d)),
+        position: new CANNON.Vec3(p.x, p.y, p.z),
+        quaternion: new CANNON.Quaternion(q?.x || 0, q?.y || 0, q?.z || 0, q?.w || 1)
+    })
+}
+
+const createBodies = (conf) => {
+    return conf.map(createBody)
+}
+
 export default class StaticEntities {
     constructor() {
-        this.bodies = [
+        this.bodies = createBodies([
             // Floor
-            this.floor = new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(8, 1, 8)),
-                position: new CANNON.Vec3(0, -1, 0)
-            }),
+            { s: { 'w': 8, 'h': 1, 'd': 8 }, p: { 'x': 0, 'y': -1, 'z': 0 } },
             // Couch
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(1.605, 0.275, 0.525)),
-                position: new CANNON.Vec3(2.79, 0.25, -2.95),
-                quaternion: new CANNON.Quaternion(0.0, 0.0, -0.0, 1.0)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(1.605, 0.235, 0.22)),
-                position: new CANNON.Vec3(2.79, 0.76, -3.33),
-                quaternion: new CANNON.Quaternion(0.0, 0.0, -0.0, 1.0)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(0.53, 0.235, 1.08)),
-                position: new CANNON.Vec3(3.83, 0.27, -2.43),
-                quaternion: new CANNON.Quaternion(0.0, 0.0, -0.0, 1.0)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(0.215, 0.53, 1.08)),
-                position: new CANNON.Vec3(4.16, 0.48, -2.43),
-                quaternion: new CANNON.Quaternion(0.0, 0.0, -0.0, 1.0)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(0.075, 0.375, 0.5)),
-                position: new CANNON.Vec3(1.22, 0.45, -2.94),
-                quaternion: new CANNON.Quaternion(0.0, 0.0, -0.0, 1.0)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.375, 0.075)),
-                position: new CANNON.Vec3(3.82, 0.45, -1.34),
-                quaternion: new CANNON.Quaternion(0.0, 0.0, -0.0, 1.0)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(.5, .25, .5)),
-                position: new CANNON.Vec3(1.6, .25, -1.3),
-            }),
-            // TV
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(.5, .35, .2)),
-                position: new CANNON.Vec3(2.62, 1.06, 1.37),
-                quaternion: new CANNON.Quaternion(0, -0.976296, 0, 0.2164396)
-            }),
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.425, 0.35)),
-                position: new CANNON.Vec3(2.63, 0.35, 1.39),
-                quaternion: new CANNON.Quaternion(-0.15304601192474365, 0.6903454661369324, -0.6903455853462219, -0.15304598212242126)
-            }),
-            // Fan
-            new CANNON.Body({
-                type: CANNON.Body.STATIC,
-                shape: new CANNON.Box(new CANNON.Vec3(0.8, 0.25, 0.8)),
-                position: new CANNON.Vec3(3, 3.7, -2),
-            }),
-        ]
+            { s: { 'w': 1.605, 'h': 0.275, 'd': 0.525 }, p: { 'x': 2.79, 'y': 0.25, 'z': -2.95 } },
+            { s: { 'w': 1.605, 'h': 0.235, 'd': 0.22 }, p: { 'x': 2.79, 'y': 0.76, 'z': -3.33 } },
+            { s: { 'w': 0.53, 'h': 0.235, 'd': 1.08 }, p: { 'x': 3.83, 'y': 0.27, 'z': -2.43 } },
+            { s: { 'w': 0.215, 'h': 0.53, 'd': 1.08 }, p: { 'x': 4.16, 'y': 0.48, 'z': -2.43 } },
+            { s: { 'w': 0.075, 'h': 0.375, 'd': 0.5 }, p: { 'x': 1.22, 'y': 0.45, 'z': -2.94 } },
+            { s: { 'w': 0.5, 'h': 0.375, 'd': 0.075 }, p: { 'x': 3.82, 'y': 0.45, 'z': -1.34 } },
+            { s: { 'w': 0.5, 'h': 0.25, 'd': 0.5 }, p: { 'x': 1.6, 'y': 0.25, 'z': -1.3 } },
+            // // TV
+            { s: { 'w': 0.5, 'h': 0.35, 'd': 0.2 }, p: { 'x': 2.62, 'y': 1.06, 'z': 1.37 }, q: { 'x': 0, 'y': -0.976296, 'z': 0, 'w': 0.2164396 } },
+            { s: { 'w': 0.5, 'h': 0.425, 'd': 0.35 }, p: { 'x': 2.63, 'y': 0.35, 'z': 1.39 }, q: { 'x': -0.15304601192474365, 'y': 0.6903454661369324, 'z': -0.6903455853462219, 'w': -0.15304598212242126 } },
+            // // Fan
+            { s: { 'w': 0.8, 'h': 0.25, 'd': 0.8 }, p: { 'x': 3, 'y': 3.7, 'z': -2 } }
+        ])
     }
 }
--- scripts/blender.py
+++ scripts/blender.py
@@ -15,6 +15,9 @@ def q_blender_to_three(w, x, y, z):
 def b_blender_to_three(w, d, h):
     return Box(*tuple(d / 2 for d in (w, h, d)))
 
+def dformat(nt):
+    return dict(nt._asdict())
+
 with open('./out.txt', 'w') as f:
     print('generating physics bodies')
     for obj in bpy.context.selected_objects:
@@ -22,13 +25,4 @@ with open('./out.txt', 'w') as f:
         s = b_blender_to_three(*tuple(round(c, 2) for c in obj.dimensions))
         v = v_blender_to_three(*tuple(round(c, 2) for c in obj.location))
         q = q_blender_to_three(*tuple(obj.rotation_quaternion))
-        data = '\n'.join([
-            "new CANNON.Body({",
-            "\ttype: CANNON.Body.STATIC,",
-            f"\tshape: new CANNON.Box(new CANNON.Vec3({s.w}, {s.h}, {s.d})),",
-            f"\tposition: new CANNON.Vec3({v.x}, {v.y}, {v.z}),",
-            f"\tquaternion: new CANNON.Quaternion({q.x}, {q.y}, {q.z}, {q.w})",
-            "}),",
-            ""
-        ])
-        f.write(data)
+        f.write(f"{{s: {dformat(s)}, p: {dformat(v)}, q: {dformat(q)}}},\n")
